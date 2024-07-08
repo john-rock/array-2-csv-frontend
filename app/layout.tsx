82584fe5f907126574e1c06f4d34dcec252b6a35
john-rock <johnmrock.jr@gmail.com>
2024-07-08 08:53:57 -0400

    update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
+import { Outfit as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import DotGridPattern from "@/components/DotGridPattern";
